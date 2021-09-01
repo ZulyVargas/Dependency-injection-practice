62bea139ed85e32acee0f57cdbe5f9d909ac8917
ZulyVargas <zuly.vargas@mail.escuelaing.edu.co>
2021-09-01 18:42:00 -0500

    especificación métodos

---
+++ src/main/java/hangman/model/BonusScore.java
@@ -0,0 +1,17 @@
+package hangman.model;
+
+public class BonusScore implements GameScore {
+
+	@Override
+	/**
+	 *Calcula el puntaje total segun el conteo de letras correctas e incorrectas .
+	 *@param correctCount - Número de letras correctas.
+	 *@param incorrectCount - Número de letras incorrectas.
+	 *
+	 */
+	public int calculateScore(int correctCount, int incorrectCount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/hangman/model/OriginalScore.java
@@ -0,0 +1,20 @@
+package hangman.model;
+
+public class OriginalScore implements GameScore {
+
+	// @pre, @pos, @param, @throws 
+	
+	/**
+	 *Calcula el puntaje total segun el conteo de letras incorrectas.
+	 *
+	 *@param correctCount - Número de letras correctas.
+	 *@param incorrectCount - Número de letras incorrectas.
+	 *
+	 */
+	@Override
+	public int calculateScore(int correctCount, int incorrectCount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
